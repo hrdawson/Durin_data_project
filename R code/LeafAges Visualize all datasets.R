aed0308b5a4055f306ff5d420550d6dfc4d49667
Hilary Rose Dawson <hrosedawson@gmail.com>
2023-10-16 19:56:58 -0700

    Cleaned up to match current DURIN object and reworked graph to just Sogndal

--- R code/LeafAges Visualize all datasets.R
+++ R code/LeafAges Visualize all datasets.R
@@ -2,47 +2,33 @@
 
 # Make big tibble ----
 DURIN.lit = durin |>
-  # read.csv("output/2023.09.11_cleanDURIN.csv") %>%
-  select(-dry_mass_g) |>
-  # Add in prelim dry mass data
-  left_join(read.csv("raw_data/2023.09.11_DURIN_drymass.csv")) |>
   # Filter to relevant data
   filter(species %in% c("Vaccinium vitis-idaea", "Empetrum nigrum")) |>
-  # Filter out erroneous leaf scans
-  filter(!envelope_ID %in% c("ATG1962", "AVY7377", "AVI9865", "AXY0067")) |>
-  # filter(siteID == "Lygra") |>
-  # filter(project == "Field - Traits") |>
-  # filter(DroughtTrt %in% c(NA, "Amb (0)")) |>
+  filter(siteID == "Sogndal") |>
+  filter(project == "Field - Traits") |>
   # Add DURIN field
-  mutate(dataset = "DURIN",
-         # Calculate SLA with dry weights
-         SLA.wet = leaf_area/wet_mass_g,
-         # Calculate SLA with dry weights
-         SLA.dry = leaf_area/dry_mass_g,
-         # Calculate LDMC
-         LDMC = (dry_mass_g*1000)/wet_mass_g) |>
+  mutate(dataset = "DURIN") |>
   # Select columns for quick comparison
-  relocate(c(leaf_area, bulk_nr_leaves_clean, SLA.wet, SLA.dry, dry_mass_g, LDMC), .after = plant_height) |>
+  relocate(c(leaf_area, SLA, dry_mass_g, wet_mass_g, LDMC, leaf_thickness_1_mm:leaf_thickness_3_mm), .after = leaf_age) |>
   select(envelope_ID, species, dataset, leaf_age:leaf_thickness_3_mm) |>
-  select(-bulk_nr_leaves) |>
-  # Calculate individual leaf values
-  mutate(leaf_area = leaf_area/bulk_nr_leaves_clean,
-         wet_mass_g = wet_mass_g/bulk_nr_leaves_clean,
-         dry_mass_g = dry_mass_g/bulk_nr_leaves_clean) |>
-  # Rename SLA for now
-  # rename(SLA = SLA.wet) |>
-  rename(SLA = SLA.dry) |>
   # Tidy in long form
-  pivot_longer(cols = plant_height:leaf_thickness_3_mm, names_to = "trait", values_to = "value") |>
+  pivot_longer(cols = leaf_area:leaf_thickness_3_mm, names_to = "trait", values_to = "value") |>
   # Standardize traits
   mutate(trait = replace(trait,
                          trait == "leaf_thickness_1_mm" | trait == "leaf_thickness_2_mm" | trait == "leaf_thickness_3_mm",
                          "leaf_thickness")) |>
-  filter(!trait %in% c("bulk_nr_leaves_clean", "plant_height")) |>
   # Add in external datasets
   bind_rows(tundratraits.join, leda.join, trydata) |>
   bind_rows(LitReview.datasets |> select(dataset, leaf_age, trait, value.converted, species) |>
               rename(value = value.converted)) |>
+  filter(!trait %in% c("bulk_nr_leaves_clean", "plant_height")) |>
+  # Filter out erroneous values
+  mutate(value = case_when(
+    trait == "wet_mass_g" & value > 0.1 ~ NA,
+    trait == "wet_mass_g" & species == "Empetrum nigrum" & value > 0.02 ~ NA,
+    trait == "dry_mass_g" & species == "Empetrum nigrum" & value > 0.002 ~ NA,
+    TRUE ~ value
+  )) |>
   # Standardize leaf_ages to reduce chaos
   mutate(leaf_age = case_when(
     leaf_age == "database" ~ "unspecified",
@@ -54,11 +40,11 @@ DURIN.lit = durin |>
   dataset = factor(dataset, levels = c("DURIN", "Literature", "LEDA", "TRY", "TTT"),
                    labels = c("DURIN", "Literature", "Database", "Database", "Database")),
   trait = factor(trait, levels = c("leaf_area", "SLA", "LDMC", "dry_mass_g", "leaf_thickness",
-                                   "wet_mass_g", "SLA.wet"),
+                                   "wet_mass_g"),
                  labels = c("Leaf area (cm^2)", "SLA (cm^2/g)", "LDMC (mg/g)", "Dry mass (g)",
-                            "Leaf thickness (mm)", "Wet mass (g)", "SLA (cm^2/g)"))) |>
+                            "Leaf thickness (mm)", "Wet mass (g)"))) |>
   # Remove plant height measurements
-  filter(trait != "plant_height") |>
+  # filter(trait != "plant_height") |>
   # Drop traits that can't be calculated yet
   drop_na(value)
 
@@ -148,7 +134,8 @@ ggplot(DURIN.lit |> filter(trait == "LDMC") |>
   theme_bw()
 
 ## Three traits together ----
-ggplot(DURIN.lit |> filter(trait %in% c("Leaf area (cm^2)", "SLA (cm^2/g)", "LDMC (mg/g)")) |>
+ggplot(DURIN.lit |>
+         filter(trait %in% c("Leaf area (cm^2)", "SLA (cm^2/g)", "LDMC (mg/g)")) |>
          drop_na(leaf_age),
        aes(x = leaf_age, y = value,fill = dataset, color = dataset)) +
   geom_boxplot(outlier.shape = NA, alpha = 0.5) +
@@ -165,4 +152,4 @@ ggplot(DURIN.lit |> filter(trait %in% c("Leaf area (cm^2)", "SLA (cm^2/g)", "LDM
         ggh4x.facet.nestline = element_line(colour = "black"),
         axis.text.x = element_text(angle = 45, vjust = 0.5, hjust = 0.5))
 
-ggsave("visualizations/2023.09.27_TraitsTogether.png", width = 10, height = 8, units = "in")
+ggsave("visualizations/2023.10.16_TraitsTogether_Sogndal.png", width = 10, height = 8, units = "in")
