cd56136586b4bc06d0232141b5fcad195ab63f4d
hrdawson <hrosedawson@gmail.com>
2024-01-01 09:44:02 +1100

    Joined resampled data to envelope IDs

--- R code/d13C traitstrap.R
+++ R code/d13C traitstrap.R
@@ -1,6 +1,8 @@
 library(traitstrap)
 library(tidyverse)
 library(tidylog)
+# For randomisation below
+library(data.table)
 
 # Check the vignette for tips https://cran.r-project.org/web/packages/traitstrap/vignettes/traitstrap-workflow.html
 
@@ -50,3 +52,35 @@ ts.d13C.nonPar <- trait_np_bootstrap(
 ggplot(ts.d13C.nonPar, aes(x = value, fill = species))  +
   geom_histogram() +
   theme_bw()
+
+# Randomly resample bootstrapped data with DURIN leaves ----
+set.seed(2023)
+
+## Make a list of all envelope_ID values I need ----
+ts.envelopes = durin |>
+  # Filter to relevant data
+  filter(species %in% c("Vaccinium vitis-idaea", "Empetrum nigrum")) |>
+  filter(siteID == "Sogndal") |>
+  filter(project == "Field - Traits") |>
+  select(envelope_ID, species) |>
+  # Venturing into data.table (Jonas, wish me luck...)
+  data.table()
+
+ts.data = ts.d13C.nonPar |>
+  ungroup() |>
+  select(species, value) |>
+  data.table()
+
+# Code from https://stackoverflow.com/questions/56559559/join-data-frames-and-select-random-row-when-there-are-multiple-matches
+ts.d13C.random =
+  ts.data[ts.envelopes, on = .(species),
+                    {ri <- sample(.N, 1L)
+                    .(envelope_ID = envelope_ID,
+                      value = value[ri])}, by = .EACHI]
+
+# It's quite likely this isn't actually random resampling
+# But it works for now
+# And there is an issue open to make it better in the future
+# Also, the shape of the sampled data roughly matches the shape of the bootstrapped data
+
+
