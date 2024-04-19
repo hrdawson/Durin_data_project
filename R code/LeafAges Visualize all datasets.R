8182ad6db6830e15d671688abab080adc35a8ea3
hrdawson <hrosedawson@gmail.com>
2024-04-19 07:04:54 -0700

    Brought back in outlier code, added in summary code, added in coverage code

--- R code/LeafAges Visualize all datasets.R
+++ R code/LeafAges Visualize all datasets.R
@@ -83,6 +83,14 @@ DURIN.lit = durin |>
   mutate(trait = replace(trait,
                          trait == "leaf_thickness_1_mm" | trait == "leaf_thickness_2_mm" | trait == "leaf_thickness_3_mm",
                          "leaf_thickness")) |>
+  # replace outliers with NA
+  mutate(value = case_when(
+    trait == "SLA" & value > 400 ~ NA,
+    trait == "wet_mass_g" & species == "Empetrum nigrum" & value > 0.005 ~ NA,
+    trait == "dry_mass_g" & species == "Empetrum nigrum" & value > 0.0025 ~ NA,
+    trait == "LDMC" & value > 600 ~ NA,
+    TRUE ~ value
+  )) |>
   # Add in external datasets
   bind_rows(tundratraits.join, leda.join, trydata) |>
   bind_rows(LitReview.datasets |> select(dataset, leaf_age, trait, value.converted, species, source) |>
@@ -103,6 +111,7 @@ DURIN.lit = durin |>
   ),
   leaf_age = factor(leaf_age, levels = c("young", "old", "unspecified"),
                     labels = c("current", "previous", "unspecified")),
+  database = dataset,
   dataset = factor(dataset, levels = c("DURIN", "Literature", "LEDA", "TRY", "TTT"),
                    labels = c("DURIN", "Literature", "Database", "Database", "Database")),
   trait = factor(trait, levels = c("leaf_area", "SLA", "LDMC", "dry_mass_g", "leaf_thickness",
@@ -110,9 +119,16 @@ DURIN.lit = durin |>
                  labels = c("Leaf area (cm^2)", "SLA (cm^2/g)", "LDMC (mg/g)", "Dry mass (g)",
                             "Leaf thickness (mm)", "Wet mass (g)"))) |>
   # Remove plant height measurements
-  # filter(trait != "plant_height") |>
+  filter(!(trait %in% c("plant_height", "Wet mass (g)", "Leaf thickness (mm)"))) |>
   # Drop traits that can't be calculated yet
   drop_na(value)
+
+## Summarise sources
+DURIN.lit.sources.sum = DURIN.lit |>
+  group_by(database, species, trait) |>
+  summarise(n = length(dataset)) |>
+  pivot_wider(names_from = species, values_from = n) |>
+  write.csv("output/2024.04.18_DURIN.lit.sources.csv")
 
 ## Find NA leaf_age
 error.leafage = DURIN.lit |>
@@ -130,7 +146,7 @@ library(patchwork)
 
 ## Three traits together ----
 ggplot(DURIN.lit |>
-         filter(trait %in% c("Leaf area (cm^2)", "SLA (cm^2/g)", "LDMC (mg/g)")) |>
+         filter(trait %in% c("SLA (cm^2/g)", "Leaf area (cm^2)", "Dry mass (g)", "LDMC (mg/g)")) |>
          drop_na(leaf_age),
        aes(x = leaf_age, y = value,fill = dataset, color = dataset)) +
   geom_boxplot(outlier.shape = NA, alpha = 0.5) +
@@ -147,4 +163,57 @@ ggplot(DURIN.lit |>
         ggh4x.facet.nestline = element_line(colour = "black"),
         axis.text.x = element_text(angle = 45, vjust = 0.5, hjust = 0.5))
 
-# ggsave("visualizations/2023.10.16_TraitsTogether_Sogndal.png", width = 10, height = 8, units = "in")
+ggsave("visualizations/2024.04.18_TraitsTogether_Sogndal.png", width = 12, height = 6, units = "in")
+
+## Calculate coverage ---
+## Code is from Julia and Pekka's paper
+## https://github.com/poniitty/ITV_grids/blob/main/scripts/09_global_versus_local_ITV.R
+
+perce_leafAge <- DURIN.lit %>%
+  group_by(species, trait) %>%
+  mutate(maxv = max(value)) %>%
+  group_by(species, leaf_age, trait, dataset) %>%
+  summarise(range = round(max(value)-min(value),3),
+            maxv = max(maxv)) |>
+  pivot_wider(id_cols = c(species,trait,leaf_age, maxv), names_from = dataset, values_from = range) |>
+  # Bring dataset data to DURIN
+  group_by(species, trait) |>
+  fill(Database, .direction = "updown") |>
+  drop_na(DURIN) |>
+  mutate(diff = round(DURIN/Database*100)) %>%
+  mutate(diff = paste0(diff,"%")) |>
+  mutate(dataset = "DURIN")
+
+perce <- DURIN.lit %>%
+  group_by(species, trait) %>%
+  mutate(maxv = max(value)) %>%
+  group_by(species, trait, dataset) %>%
+  summarise(range = round(max(value)-min(value),3),
+            maxv = max(maxv)) |>
+  pivot_wider(id_cols = c(species,trait,maxv), names_from = dataset, values_from = range) |>
+  # Bring dataset data to DURIN
+  group_by(species, trait) |>
+  fill(Database, .direction = "updown") |>
+  drop_na(DURIN) |>
+  mutate(diff = round(DURIN/Database*100)) %>%
+  mutate(diff = paste0(diff,"%"))
+
+## Three traits together ----
+ggplot(DURIN.lit |>
+         filter(trait %in% c("SLA (cm^2/g)", "Leaf area (cm^2)", "Dry mass (g)", "LDMC (mg/g)")) |>
+         drop_na(leaf_age),
+       aes(x = leaf_age, y = value,fill = dataset, color = dataset)) +
+  geom_boxplot(outlier.shape = NA, alpha = 0.5) +
+  geom_point(position = position_jitterdodge(), alpha = 0.5) +
+  geom_text(data = perce_leafAge, aes(label=diff, y = maxv, color = "black"), vjust=1, size=4, color = "black") +
+  scale_x_discrete(guide = "axis_nested") +
+  scale_fill_viridis(discrete=T) +
+  scale_color_viridis(discrete=T) +
+  # scale_y_log10() +
+  facet_nested(~ species + trait, scales = "free", independent = "y",
+               nest_line = element_line(linetype = 2)) +
+  labs(x = "Leaf year") +
+  theme_bw() +
+  theme(strip.background = element_blank(),
+        ggh4x.facet.nestline = element_line(colour = "black"),
+        axis.text.x = element_text(angle = 45, vjust = 0.5, hjust = 0.5))
