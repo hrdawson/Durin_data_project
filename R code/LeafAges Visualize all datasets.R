a34e36d0cf5ab53a1254c3d0b69728d87d339b27
hrdawson <hrosedawson@gmail.com>
2024-05-07 10:56:46 -0700

    Shifted prep code to `load data.R`

--- R code/LeafAges Visualize all datasets.R
+++ R code/LeafAges Visualize all datasets.R
@@ -24,34 +24,8 @@ DURIN.database = tundratraits.join |>
 # write.csv(DURIN.database, "clean_data/DURIN_database.data.csv")
 
 # Make big tibble ----
-durin = read.csv("clean_data/DURIN_clean.csv")
 
-DURIN.lit = durin |>
-  # Filter to relevant data
-  filter(species %in% c("Vaccinium vitis-idaea", "Empetrum nigrum")) |>
-  filter(siteID == "Sogndal") |>
-  filter(project == "Field - Traits") |>
-  # Add DURIN field
-  mutate(dataset = "DURIN",
-         source = "DURIN") |>
-  # Select columns for quick comparison
-  relocate(c(leaf_area, SLA, dry_mass_g, wet_mass_g, LDMC, leaf_thickness_1_mm:leaf_thickness_3_mm), .after = leaf_age) |>
-  select(envelope_ID, species, dataset, habitat, source, leaf_age:leaf_thickness_3_mm) |>
-  # Tidy in long form
-  pivot_longer(cols = leaf_area:leaf_thickness_3_mm, names_to = "trait", values_to = "value") |>
-  # Standardize traits
-  mutate(trait = replace(trait,
-                         trait == "leaf_thickness_1_mm" | trait == "leaf_thickness_2_mm" | trait == "leaf_thickness_3_mm",
-                         "leaf_thickness")) |>
-  # replace outliers with NA
-  mutate(value = case_when(
-    trait == "SLA" & value > 400 ~ NA,
-    trait == "wet_mass_g" & species == "Empetrum nigrum" & value > 0.005 ~ NA,
-    trait == "dry_mass_g" & species == "Empetrum nigrum" & value > 0.0025 ~ NA,
-    trait == "LDMC" & value > 600 ~ NA,
-    TRUE ~ value
-  )) |>
-  drop_na(value) |>
+DURIN.lit = leaf.ages |>
   # Add in external datasets
   bind_rows(tundratraits.join, leda.join, trydata) |>
   bind_rows(LitReview.datasets |> select(dataset, leaf_age, trait, value.converted, species, source) |>
